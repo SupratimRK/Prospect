d56b975813f75841bbd29f99a8d780d25263bf42
Supratim <supratimrk@gmail.com>
2022-09-20 22:08:31 +0530

    Added favicon

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Prospect | A Realtime Chat App</title>
+  <link rel="shortcut icon" type="image/png" href="logo.png">
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
 </head>
